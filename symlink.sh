0fa3fbb8bd342584bb3149a3760264a6d00ccf7d
Stephen Kirk <me@thestephen.net>
2018-06-08 11:07:02 +0200

    add vs code symlinking

--- symlink.sh
+++ symlink.sh
@@ -2,3 +2,6 @@
 
 ln -s ~/dotfiles/.* ~/
 rm -rf ~/.git # Remove accidental git folder committing
+
+# symlink vs code since it uses some really weird path
+ln -s "~/dotfiles/vscode_settings.json" "~/Library/Application Support/Code/User/settings.json"
