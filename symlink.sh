c143a768316b50c26510128b54c2278cb4d22ed7
stephenkirk <me@thestephen.net>
2018-06-20 15:55:19 +0200

    Update symlink file

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,9 @@
 # Lazy dotfile syncing until I can get something going that might not accidentally blow up my computer
 
+# Symlink all dotfiles
 ln -s ~/dotfiles/.* ~/
 rm -rf ~/.git # Remove accidental git folder committing
+
+# Symlink VS Code bindings
+ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
