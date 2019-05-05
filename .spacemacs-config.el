78a8441579c41165aa58c09ed49474a49e92f8d5
Stephen Kirk <me@thestephen.net>
2019-05-05 15:21:39 +0200

    comment out variable pitch; delete stpb

--- .spacemacs-config.el
+++ .spacemacs-config.el
@@ -1,13 +1,3 @@
-
-;; (defun switch-to-previous-buffer ()
-;;   "Switch to previously open buffer.
-;; Repeated invocations toggle between the two most recently open buffers."
-;;   (interactive)
-;;   (switch-to-buffer (other-buffer (current-buffer) 1)))
-
-;; (global-set-key (kbd "M-x b") 'switch-to-previous-buffer)
-
-
 (defun config-file ()
   "Return the path to this config file."
   "~/.spacemacs-config.el"
@@ -32,14 +22,13 @@
 (evil-leader/set-key
   "feo" 'find-org-file)
 
-
 (evil-leader/set-key
   "s!" 'shell)
 
 (add-hook 'org-mode-hook
           '(lambda ()
              ;; (setq line-spacing 0.2) ;; Add more line padding for readability
-             (variable-pitch-mode 1) ;; All fonts with variable pitch.
+             ;; (variable-pitch-mode 1) ;; All fonts with variable pitch.
              (mapc
               (lambda (face) ;; Other fonts with fixed-pitch.
                 (set-face-attribute face nil :inherit 'fixed-pitch))
@@ -53,7 +42,3 @@
                     'org-meta-line
                     'org-document-info-keyword))))
 
-
-
-;; Make fullscreen great again
-(setq ns-use-native-fullscreen nil)
