7f3a11426549708a13fc01a2d9c154f68ed58443
stephenkirk <me@thestephen.net>
2019-08-10 18:22:40 +0200

    Add symlink for spectacle

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,12 @@
 # Lazy dotfile syncing until I can get something going that might not accidentally blow up my computer
 
-# Symlink all dotfiles
+# dotfiles
 ln -s ~/dotfiles/.* ~/
 rm -rf ~/.git # Remove accidental git folder committing
 
-# Symlink VS Code bindings
+# vs code
 ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
+# spectacle
+ln -s ~/dotfiles/spectacle_shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
