d660f9a73e8acd815cea5ab7bc9bbc019241aa4e
stephenkirk <me@thestephen.net>
2020-01-24 12:11:52 +0100

    remove vscodium symlinks

--- symlink.sh
+++ symlink.sh
@@ -4,12 +4,10 @@
 ln -s ~/dotfiles/.* ~/
 rm -rf ~/.git # Remove accidental git folder committing
 
-# vs code
-# ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
-# ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/VSCodium/User/settings.json
-ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/VSCodium/User/keybindings.json
 
+# vs code
+ ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
+ ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # spectacle
 ln -s ~/dotfiles/spectacle_shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
