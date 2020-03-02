9bf4a0c0c81d09bae48e0fa40f008ee420500e38
stephenkirk <me@thestephen.net>
2020-03-02 18:04:17 +0100

    Update symlinks

--- symlink.sh
+++ symlink.sh
@@ -1,15 +1,16 @@
-# Lazy dotfile syncing until I can get something going that might not accidentally blow up my computer
+ln -s -f ~/dotfiles/.hammerspoon ~/.hammerspoon
+ln -s -f ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -s -f ~/dotfiles/.org-mode.el ~/.org-mode.el
+ln -s -f ~/dotfiles/.spacemacs ~/.spacemacs
+ln -s -f ~/dotfiles/.spacemacs-config.el ~/.spacemacs-config.el
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -fs ~/dotfiles/spectacle_shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
 
-# dotfiles
-ln -s ~/dotfiles/.* ~/
-rm -rf ~/.git # Remove accidental git folder committing
-
-
-# vs code
- ln -s ~/dotfiles/vscode_settings.json ~/Library/Application\ Support/Code/User/settings.json
- ln -s ~/dotfiles/vscode_keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-
-# spectacle
-ln -s ~/dotfiles/spectacle_shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
-
-ln -s ~/Dropbox/dotfiles_private ~/
+ln -fs ~/Dropbox/dotfiles_private ~/
+ln -fs ~/Dropbox/dotfiles_private/.profile ~/.profile
+ln -fs ~/Dropbox/org ~/
+ln -fs ~/Dropbox/dotfiles_private/bin ~/bin
