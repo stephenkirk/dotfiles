c414dcab44a694ff4915e46041cb3947007e798a
stephenkirk <me@thestephen.net>
2020-05-29 00:59:21 +0200

    Move everything out of .spacemacs

--- .spacemacs-config.el
+++ .spacemacs-config.el
@@ -46,3 +46,64 @@ same directory as the org-buffer and insert a link to this file."
 
 (setq solarized-use-variable-pitch nil) ;; don't use weird fonts
 (setq multi-term-program "/usr/bin/zsh")
+
+(setq-default dotspacemacs-configuration-layers '(
+                                                  yaml
+                                                  html
+                                                  csv
+                                                  javascript
+                                                  csharp
+                                                  helm
+                                                  markdown
+                                                  git
+                                                  auto-completion
+                                                  org
+                                                  (osx :variables osx-option-as 'meta
+                                                                  osx-right-option-as 'none)
+                                                  )
+)
+
+;; on one of my computers, melpa as a package endpoint doesn't exist for some reason
+(require 'package)
+(add-to-list 'package-archives
+              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; (package-refresh-contents)
+
+dotspacemacs-search-tools '("rg" "ag" "pt" "ack" "grep")
+(setq dotspacemacs-additional-packages '(org-clock-csv org-plus-contrib solarized-theme helm-rg))
+(setq dotspacemacs-excluded-packages '(powerline magit-gitflow yasnippet))
+
+;; load dotfiles
+(load "~/.spacemacs-config.el") ;; spacemacs config
+(load "~/.org-mode.el") ;; org-related config
+
+;; private config shared from dropbox
+;; where i keep all my custom configuration that i don't want to share in version control
+;; for example, i have different org files for each client i work with in my org folder
+;; this allows me to have bindings such as "go to org file for client", without cluttering my dotfiles
+(mapc 'load (file-expand-wildcards "~/Dropbox/dotfiles_private/*.el"))
+
+(defun dotspacemacs/user-config ()
+  "Configuration function for user code."
+  ;; make evil-mode up/down operate in screen lines instead of logical lines
+  (define-key evil-motion-state-map "j" 'evil-next-visual-line)
+  (define-key evil-motion-state-map "k" 'evil-previous-visual-line)
+  ;; ..also in visual mode
+  (define-key evil-visual-state-map "j" 'evil-next-visual-line)
+  (define-key evil-visual-state-map "k" 'evil-previous-visual-line)
+  )
+
+;; spacemacs speedup hack
+(setq auto-window-vscroll nil)
+
+;; Disable unicode symbols in mode line to speed up performance
+(setq dotspacemacs-mode-line-unicode-symbols nil)
+
+;; Option modifier - right option key for symbols; left for meta
+(setq-default mac-right-option-modifier nil)
+
+;; Make fullscreen sane
+;; TODO: classic iterm style fullscreen
+(setq-default dotspacemacs-fullscreen-use-non-native nil)
+(setq-default dotspacemacs-maximized-at-startup t)
+(setq-default ns-use-native-fullscreen nil)
