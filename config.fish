c578d8a3697e14d36c8e538a029f310a5a934019
Stephen Kirk <me@thestephen.net>
2023-04-24 16:15:20 +0200

    add fish settings to dotfiles

---
+++ config.fish
@@ -0,0 +1,42 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -U fish_greeting
+
+# Environment variables
+set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
+set -gx LC_ALL en_US.UTF-8
+set -gx LANG en_US.UTF-8
+set -gx EDITOR vim
+set -gx DOTNET_ROOT $HOME/.dotnet/dotnet
+
+# Paths
+set -Ua fish_user_paths $HOME/bin
+set -Ua fish_user_paths /opt/homebrew/bin
+set -Ua fish_user_paths $HOME/.dotnet/dotnet
+set -Ua fish_user_paths $HOME/.dotnet/tools
+set -Ua fish_user_paths $HOME/.emacs.d/bin
+set -Ua fish_user_paths $HOME/Library/Python/3.8/bin
+
+# vi mode in shell
+fish_vi_key_bindings
+set -U fish_escape_delay_ms 10
+
+# fzf config
+set -gx FZF_DEFAULT_OPTS "--height 50% --layout=reverse --border --inline-info"
+set -gx FZF_DEFAULT_COMMAND 'rg --files'
+set -gx FZF_CTRL_T_COMMAND "rg --files --hidden --follow --no-messages"
+
+# aliases
+alias dir='pwd'
+alias where='grealpath'
+alias tree='tree -C' # Colored trees by default
+alias pt='papertrail'
+alias rc='vim ~/.config/fish/config.fish'
+
+alias gs='git status'
+alias gca='git commit -a'
+alias gc='git commit'
+alias grc='git rebase --continue'
+alias gpl='git pull'
