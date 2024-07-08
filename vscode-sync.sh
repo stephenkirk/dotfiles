10fe2dc525c94f9000561a45b04da24db611434b
Stephen Kirk <me@thestephen.net>
2024-07-08 13:42:39 +0200

    remove extension install from vs code script, keep in brewfile instead

--- vscode-sync.sh
+++ vscode-sync.sh
@@ -6,18 +6,5 @@ mkdir -p ~/Library/Application\ Support/Code/User
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
-# Install extensions
-extensions=(
-    "ms-python.python"
-    "dbaeumer.vscode-eslint"
-    "esbenp.prettier-vscode"
-    "asvetliakov.vscode-neovim"
-)
-
-for extension in "${extensions[@]}"
-do
-    code --install-extension "$extension"
-done
-
 echo "VS Code settings, keybindings, and extensions have been synchronized."
 
