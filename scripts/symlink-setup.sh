6e5a5219c9bb49759b7dff6ba2c70c4a8e50ac98
Stephen Kirk <me@thestephen.net>
2024-11-20 20:27:45 +0100

    new .config dir structure

--- scripts/symlink-setup.sh
+++ scripts/symlink-setup.sh
@@ -16,10 +16,16 @@ create_symlink ~/dotfiles/.vimrc ~/.vimrc
 create_symlink ~/dotfiles/.zshrc ~/.zshrc
 
 # Config files in .config directory
+mkdir -p ~/.config/
 mkdir -p ~/.config/fish
-create_symlink ~/dotfiles/config.fish ~/.config/fish/config.fish
-create_symlink ~/dotfiles/karabiner ~/.config
-create_symlink ~/dotfiles/amethyst ~/.config
+
+create_symlink ~/dotfiles/.config/config.fish ~/.config/fish/config.fish
+create_symlink ~/dotfiles/.config/karabiner ~/.config/karabiner
+create_symlink ~/dotfiles/.config/amethyst ~/.config/amethyst
+create_symlink ~/dotfiles/.config/linearmouse ~/.config/linearmouse
+
+create_symlink ~/dotfiles/.config/zed/keymap.json ~/.config/zed/keymap.json
+create_symlink ~/dotfiles/.config/zed/settings.json ~/.config/zed/settings.json
 
 # Private dotfiles
 create_symlink ~/Documents/dotfiles_private ~/dotfiles_private
