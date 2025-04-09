782909fe2b1bdc8bb14d659c7036affd64ebd438
Stephen Kirk <me@thestephen.net>
2025-04-09 13:45:04 +0200

    🎧 Hammerspoon: Fix flaky AirPods switching with Super+A toggle magic

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,24 +1,24 @@
-local super = {"ctrl", "alt", "cmd", "shift"}
+local super = { "ctrl", "alt", "cmd", "shift" }
 
 -- Toggle dark mode from menu bar
 local function systemSetDm(state)
-  return hs.osascript.javascript(
-    string.format(
-      "Application('System Events').appearancePreferences.darkMode.set(%s)",
-      state
+    return hs.osascript.javascript(
+        string.format(
+            "Application('System Events').appearancePreferences.darkMode.set(%s)",
+            state
+        )
     )
-  )
 end
 
 local function dmIsOn()
-  local _, darkModeState = hs.osascript.javascript(
-    'Application("System Events").appearancePreferences.darkMode()'
-  )
-  return darkModeState
+    local _, darkModeState = hs.osascript.javascript(
+        'Application("System Events").appearancePreferences.darkMode()'
+    )
+    return darkModeState
 end
 
 function setDm(state)
-	systemSetDm(state)
+    systemSetDm(state)
     if state then
         darkmode:setTitle("☾")
     else
@@ -39,23 +39,30 @@ end
 -- END dark mode
 
 function killWorkApps()
-  local appsToKill = {"Firefox", "Slack", "Notion", "iTerm", "Rider", "Webstorm", "VS Code"}
-  for _, appName in ipairs(appsToKill) do
-    local app = hs.application.find(appName)
-    if app then
-      app:kill()
+    local appsToKill = { "Firefox", "Slack", "Notion", "iTerm", "Rider", "Webstorm", "VS Code" }
+    for _, appName in ipairs(appsToKill) do
+        local app = hs.application.find(appName)
+        if app then
+            app:kill()
+        end
     end
-  end
 end
 
 -- Bindings
 hs.hotkey.bind(super, "R", function() hs.reload() end)
 hs.hotkey.bind(super, "D", function() darkmodeClicked() end)
 hs.hotkey.bind(super, "X", function() killWorkApps() end)
-hs.hotkey.bind(super, "A", function() 
-	hs.execute('SwitchAudioSource -u 90-9C-4A-E0-9D-33:output', true) 
-	hs.notify.new({title="Hammerspoon", informativeText="Output device set to AirPods Max"}):send()
-	print("Changing output device")
+hs.hotkey.bind(super, "A", function()
+    local currentDevice = hs.execute('/opt/homebrew/bin/SwitchAudioSource -c')
+    if currentDevice:find("MacBook Pro") then
+        local output, status = hs.execute('/opt/homebrew/bin/SwitchAudioSource -s "Stephen’s AirPods Max #2"')
+        hs.notify.new({ title = "Hammerspoon", informativeText = "Output device set to AirPods Max" }):send()
+        print("Changing output device to AirPods Max")
+    else
+        local output, status = hs.execute('/opt/homebrew/bin/SwitchAudioSource -s "MacBook Pro Speakers"')
+        hs.notify.new({ title = "Hammerspoon", informativeText = "Output device set to MacBook Pro Speakers" }):send()
+        print("Changing output device to MacBook Pro Speakers")
+    end
 end)
 
 hs.alert.show("Config loaded")
